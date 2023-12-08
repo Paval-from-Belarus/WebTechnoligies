fd9585a9ac8e95e6952c8cac8c0985fc9754f5ec
Paval-from-Belarus <star-war-sandakan@mail.ru>
2023-12-08 12:07:14 +0300

    unknown changes

--- src/test/java/by/bsuir/poit/services/impl/AuthorizationServiceImplTest.java
+++ src/test/java/by/bsuir/poit/services/impl/AuthorizationServiceImplTest.java
@@ -13,7 +13,6 @@ import by.bsuir.poit.servlets.UserDetails;
 import by.bsuir.poit.utils.AuthorizationUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
